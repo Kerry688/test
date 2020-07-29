c6fe43a76a9e0859e3f97cbb3cdc6b03040acf15
Karim Bayoumi <karim_bayoumi@yahoo.com>
2020-07-29 12:46:14 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace LogReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Browse = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Browse
+            // 
+            this.btn_Browse.Location = new System.Drawing.Point(12, 12);
+            this.btn_Browse.Name = "btn_Browse";
+            this.btn_Browse.Size = new System.Drawing.Size(75, 23);
+            this.btn_Browse.TabIndex = 1;
+            this.btn_Browse.Text = "Browse";
+            this.btn_Browse.UseVisualStyleBackColor = true;
+            this.btn_Browse.Click += new System.EventHandler(this.btn_Browse_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(889, 558);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 622);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Browse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Browse;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+ 
+namespace LogReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+    
+
+        private System.Data.DataTable read(string filepath , int ColumnCount , int RowCount)
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+
+            try
+            {
+            
+                //create a instance for the Excel object  
+                Excel.Application oExcel = new Excel.Application();
+
+                //pass that to workbook object  
+                Excel.Workbook WB = oExcel.Workbooks.Open(filepath);
+
+                // statement get the workbookname  
+                string ExcelWorkbookname = WB.Name;
+
+                // statement get the worksheet count  
+                int worksheetcount = WB.Worksheets.Count;
+
+                Excel.Worksheet wks = (Excel.Worksheet)WB.Worksheets[1];
+
+                // statement get the firstworksheetname  
+
+                string firstworksheetname = wks.Name;
+
+                //statement get the first cell value  
+
+                //Load Columns Header
+                for(int c=1; c < ColumnCount; c++)
+                {
+                    var colData = ((Excel.Range)wks.Cells[1, c]).Value;
+                    dt.Columns.Add(colData);
+                }
+                 
+                //Load Rows
+                for (int r = 2; r < RowCount; r++)
+                {
+                    for (int c = 1; c < ColumnCount; c++)
+                    {
+                        dt.Rows.Add();
+                        var rawData = ((Excel.Range)wks.Cells[r, c]).Value;
+                        dt.Rows[r-2][c-1] = rawData;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+
+            return dt;
+        }
+
+
+        private void btn_Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    System.Data.DataTable dt = read(ofd.FileName, 10, 10);
+                    dataGridView1.DataSource = dt;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
